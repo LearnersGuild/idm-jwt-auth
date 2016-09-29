03f0890117d4ca8a95f605f5fe0a57ae63411b05
Trevor Little <trevor@learnersguild.org>
2016-09-29 11:29:37 -0400

    fixing bug in cookie domain. missing '.'

--- src/utils.js
+++ src/utils.js
@@ -83,6 +83,6 @@ export function userFromJWT(lgJWT) {
 
 export function cookieOptsJWT(/* req */) {
   const secure = (process.env.NODE_ENV === 'production')
-  const domain = process.env.IDM_BASE_URL.match(/[^.]+\.(org|com|dev)$/)[0]
+  const domain = process.env.IDM_BASE_URL.match(/\.[^.]+\.(org|com|dev)$/)[0]
   return {secure, domain, httpOnly: true}
 }
